52f20de7da6bedb68de8e803071ea6233e320f3f
AmusementSPK <Spk@videotron.qc.ca>
2026-09-23 12:25:38 -0400

    Add SPK Server Manager entry point

---
+++ Manager/Program.cs
@@ -0,0 +1,11 @@
+namespace SPK.Server.Manager;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
